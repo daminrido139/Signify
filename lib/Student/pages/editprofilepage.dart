e7ec640c3e30f81ecc82585f193c2e44dc1ffefc
kabilan-7-v <kabilanvelmani@gmail.com>
2024-10-02 20:22:30 +0530

    filechanges

---
+++ lib/Student/pages/editprofilepage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Editprofilepage extends StatelessWidget {
+  const Editprofilepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(),
+    );
+  }
+}
